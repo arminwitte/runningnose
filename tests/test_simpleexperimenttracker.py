873d15003ad43f5011ebc3c14595a232842e7490
arminwitte <110226001+arminwitte@users.noreply.github.com>
2024-07-26 14:54:27 +0200

    Create test_simpleexperimenttracker.py

---
+++ tests/test_simpleexperimenttracker.py
@@ -0,0 +1,5 @@
+import simpleexperimenttracker as sit
+
+def test_simpleexperimenttracker():
+    tracker = sit.SimpleExperimentTracker()
+    assert isinstance(tracker, sit.SimpleExperimentTracker)
