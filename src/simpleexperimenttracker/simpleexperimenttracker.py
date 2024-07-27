3d896ba826e52cccab036f5bdad544e5904648f3
arminwitte <110226001+arminwitte@users.noreply.github.com>
2024-07-27 16:25:59 +0200

    Update simpleexperimenttracker.py

--- src/simpleexperimenttracker/simpleexperimenttracker.py
+++ src/simpleexperimenttracker/simpleexperimenttracker.py
@@ -1,3 +1,18 @@
+import wonderwords
+import datetime
+
 class SimpleExperimentTracker:
     def __init__(self):
+        self.job_name = self._job_name()
         pass
+
+    def _job_name(self):
+        now = str(datetime.datetime.now().isoformat())
+        date = now.split("T")[0]
+
+        r = wonderwords.RandomWord()
+        adjective = r.word(include_parts_of_speech=["adjectives"])
+
+        # generate a random word between the length of 3 and 8 characters
+        r.word(word_min_length=3, word_max_length=8)
+        
